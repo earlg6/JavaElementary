4e174d225cf1ff4060c5ae851d47218322c3d88a
Dana Kirilyuk <danakirilyuk@gmail.com>
2018-06-11 21:07:30 +0300

    Creation of a first app Add method and mssg1

--- Dana_Kirilyuk/od/ua/hillel/TheApp.java
+++ Dana_Kirilyuk/od/ua/hillel/TheApp.java
@@ -3,7 +3,7 @@ package od.ua.hillel;
 public class TheApp {
 
     public static void main (String[] args) {
-        System.out.println ("Kalispera!");
+        System.out.println ("Kalispera! ");
     }
 
 }
