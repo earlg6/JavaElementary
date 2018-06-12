dd620919e709b2670c07b1a37d002fb7a909f254
Da1nwood <36931220+Da1nwood@users.noreply.github.com>
2018-06-13 00:45:05 +0300

    First Task

---
+++ src/ua/od/hillel/nikita_petrov/MySecondApplication.java
@@ -0,0 +1,41 @@
+//package ua.od.hillel.nikita_petrov;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+// Number 1
+//public class MySecondApplication {
+//    public static void main(String[] args) {
+//        double MOON_CONST = 0.17;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите вашу массу:");
+//        Double mass = scanner.nextDouble();
+//        System.out.println("Ваш вес на луне" + MOON_CONST*mass);
+//    }
+//}
+//Number 2
+//public class MySecondApplication {
+//    public static void main(String[] args) {
+//        String string = "1lesson1lesson1lesson";
+//        char [] chars = string.toCharArray();
+//        for (int i = 0; i < chars.length; i++) {
+//            if(chars[i] == '1'){
+//                chars[i] = '2';
+//            }
+//            if (chars[i] =='2' & i != 0)
+//                System.out.print(" " +chars[i]);
+//            else System.out.print(chars[i]);
+//            }
+//
+//    }
+//}
+//Number 3
+//public class MySecondApplication {
+//    public static void main(String[] args) {
+//        System.out.println(foo());
+//        }
+//    public static int foo (int lower, int upper){
+//        Random random = new Random();
+//        return random.nextInt((lower)+upper+1);
+//    }
+//}
